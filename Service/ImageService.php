7b392a6fa17e56039f449a8bfbf44547c358cdef
李春鹏 <lcp0578@gmail.com>
2017-08-20 17:31:05 +0800

    [MOD] update image service

--- Service/ImageService.php
+++ Service/ImageService.php
@@ -3,7 +3,14 @@ namespace Kit\Bundle\OssBundle\Service;
 
 use OSS\OssClient;
 
-class ImageService
+class ImageService extends ClientService
 {
-    
+    public function upload($bucket, $object, $file)
+    {
+        try {
+            $this->getClient()->uploadFile($bucket, $object, $file);
+        }catch (\Exception $e){
+            
+        }
+    }
 }
